19ad5381507eeaee846a711a6a471087789e7f38
Thomas Vinther <tvinther1@live.dk>
2021-04-21 16:09:32 +0200

    result siden viser lidt nu

--- carpair/views/results/results.js
+++ carpair/views/results/results.js
@@ -1,4 +1,4 @@
-var resultp = document.getElementById("resultp");
+var resultp = document.getElementById("container");
 
 function testApiRequest(par1, par2) {
     var xmlhttp = new XMLHttpRequest();
@@ -24,9 +24,11 @@ function loadCars() {
             var result = JSON.parse(this.response);
             console.log(result);
 
-            for (var i = 0; i < 2; i++) {
-                resultp.innerHTML += `<div style="background-color: blue; margin-top: 10px;">Pris: ${result[i]["price"]}, Nummerplade: ${result[i]["numberplate"]} osv... (se console for flere mulige elementer)</div>`;
-            }
+			for (var i = 0; i < result.length; i++) {
+				var row = createDomRow(result, i);
+                //resultp.innerHTML += `<div style="background-color: blue; margin-top: 10px;">Pris: ${result[i]["price"]}, Nummerplade: ${result[i]["numberplate"]} osv... (se console for flere mulige elementer)</div>`;
+				resultp.innerHTML += row;
+			}
         }
     };
     xmlhttp.open("GET", `getCars`, true);
@@ -34,3 +36,59 @@ function loadCars() {
 }
 
 loadCars();
+
+
+function createDomRow(json, i) {
+	var row = `<div class="row h150">
+		<div class="col-sm-3">
+			<div class="carImg" style="background-image: url('${json[i]["thumbnail"]}'); ">
+			</div>
+		</div>
+		<div class="col-sm-9">
+			<div class="row title">
+				<div class="col-sm-8">
+					${json[i]["title"]}
+				</div>
+				<div class="col-sm-4" style="color: #01631b">
+					${json[i]["price"]}
+				</div>
+			</div>
+		<div class="row">
+			<div class="col-sm-4">
+				${json[i]["kilometer"]}.000 km
+			</div>
+			<div class="col-sm-4">
+				${json[i]["manufactured"]}
+			</div>
+			<div class="col-sm-4">
+				${json[i]["listingdate"]}
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-sm-4">
+				${json[i]["kml"]} km/l
+			</div>
+			<div class="col-sm-4">
+				${json[i]["weighttax"]},- weighttax
+			</div>
+			<div class="col-sm-4">
+				${json[i]["zipcode"]}
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-sm-4">
+				${json[i]["fuel"]}
+			</div>
+			<div class="col-sm-4">
+				${json[i]["checkupdate"]} checkupdate
+			</div>
+			<div class="col-sm-4">
+				<a target="_blank" href="${json[i]["link"]}">View on DBA.DK</a>
+			</div>
+		</div>
+		</div>
+		
+	</div>`;
+
+	return row;
+}
